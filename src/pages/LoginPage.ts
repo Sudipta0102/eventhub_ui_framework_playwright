1f0e52301a62d9dbb651e0ac62cd7dc269fe5010
Sudipta D <sudipta0102@gmail.com>
2026-06-01 03:16:53 +0530

    feat:adding locators and methods in login page

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -1,7 +1,29 @@
+import { type Locator, type Page } from "@playwright/test";
+
 export class LoginPage { 
-    constructor(private page: any) {} 
+
+    private readonly page: Page;
+    private readonly email: Locator;
+    private readonly password: Locator;
+    private readonly loginBtn: Locator;
+
+
+    constructor(page: Page) {
+        this.page = page;
+        this.email = page.locator("#email");
+        this.password = page.locator("#password");
+        this.loginBtn = page.locator("#login-btn");
+    } 
 
     async navigate() { 
-        await this.page.goto('/login'); 
+        await this.page.goto('/login');
     } 
+
+    async login(username: string, password: string){
+
+        await this.email.fill(username);
+        await this.password.fill(password);
+        await this.loginBtn.click();
+        //await this.page.waitForLoadState('networkidle');
+    }
 }
